e6ef4908ba27acc3ebf9282124f0050e28462731
Umar Schweinsteiger <umar.dev500@gmail.com>
2024-10-17 12:32:18 +0700

    make navigation bar are transparent

--- android/app/src/main/java/com/livelocation/MainActivity.kt
+++ android/app/src/main/java/com/livelocation/MainActivity.kt
@@ -2,27 +2,27 @@ package com.livelocation
 
 import android.os.Bundle
 import com.facebook.react.ReactActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
-  override fun onCreate(savedInstanceState: android.os.Bundle?) {
-    super.onCreate(savedInstanceState)
-    enableEdgeToEdge()
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Enable edge-to-edge content, if desired
+        enableEdgeToEdge(
+            navigationBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT,
+            )
+        )
+    }
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "LiveLocation"
+    override fun getMainComponentName(): String = "LiveLocation"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
--- src/screens/homeScreen/HomeScreen.tsx
+++ src/screens/homeScreen/HomeScreen.tsx
@@ -1,13 +1,22 @@
 import {HomeHeader, MapView} from '@components/organisms';
+import {useEffect, useState} from 'react';
 import {StatusBar} from 'react-native';
 
 export const HomeScreen = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <>
       <StatusBar backgroundColor={'transparent'} barStyle={'dark-content'} />
       <HomeHeader />
       {/* Mapview */}
-      <MapView />
+      {!loading && <MapView />}
     </>
   );
 };
